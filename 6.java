5681a982039ba54a1d362789d7439d4110bf266e
saianumolu <51683985+saianumolu@users.noreply.github.com>
2019-07-03 12:41:12 -0500

    Update 6.java

--- 6.java
+++ 6.java
@@ -1,1 +1,25 @@
-new
+git config --global user.name "sai"
+git config --global user.email "anumolu@gmail.com"
+git config --global push.default simple
+
+git clone <central-repo> <local-repo>
+git status
+git add <file> or git add .
+git commit -m "Commit message"
+git log
+git push
+git pull
+git branch
+git branch <newbranch>
+git checkout <branchname>
+git merge <srcbranch> <destinationbranch>
+git cherry-pick <commitID>
+
+git stash
+git stash list
+git stash apply stash@{Indexnum}
+git stash clear
+
+git reset --soft
+git reset --mixed
+git reset --sof
